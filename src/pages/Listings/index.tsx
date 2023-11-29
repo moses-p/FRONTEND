0708c4081bdf2d312862180e9a5f434a1346a41c
moses-p <mosesharuna407@gmail.com>
2023-11-29 19:28:37 +0300

    changes

--- src/pages/Listings/index.tsx
+++ src/pages/Listings/index.tsx
@@ -16,7 +16,6 @@ type Hostel = {
 const Listings: React.FC = () => {
   const [hostels, setHostels] = useState<Hostel[] | undefined>();
   const [selectedHostel, setSelectedHostel] = useState<Hostel | undefined>();
-  const [showDropdown, setShowDropdown] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchHostels = async () => {
@@ -33,41 +32,39 @@ const Listings: React.FC = () => {
 
   const handleHostelSelect = (hostel: Hostel) => {
     setSelectedHostel(hostel);
-    setShowDropdown(true);
   };
 
   return (
     <div>
       <h2>Hostel List</h2>
-      <ul>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', gap: '16px' }}>
         {hostels &&
           hostels.map((hostel) => (
-            <li key={hostel._id}>
-              <div>
-                <h3>{hostel.name}</h3>
-                <p>{hostel.description}</p>
-                <button onClick={() => handleHostelSelect(hostel)}>Select this Hostel</button>
-              </div>
-              {showDropdown && selectedHostel && selectedHostel._id === hostel._id && (
-                <div>
-                  <h2>Selected Hostel</h2>
-                  <p>Name: {selectedHostel.name}</p>
-                  <p>Description: {selectedHostel.description}</p>
-                  <h3>Room Information</h3>
-                  <ul>
-                    {selectedHostel.rooms.map((room, index) => (
-                      <li key={index}>
-                        <p>Room {index + 1}</p>
-                        <p>Standard: {room.standard}</p>
-                        <p>Price: {room.price}</p>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </li>
+            <div key={hostel._id} style={{ border: '1px solid #ccc', padding: '16px' }}>
+              <h3>{hostel.name}</h3>
+              <p>{hostel.description}</p>
+              <button onClick={() => handleHostelSelect(hostel)}>Select this Hostel</button>
+            </div>
           ))}
-      </ul>
+      </div>
+
+      {selectedHostel && (
+        <div>
+          <h2>Selected Hostel</h2>
+          <p>Name: {selectedHostel.name}</p>
+          <p>Description: {selectedHostel.description}</p>
+          <h3>Room Information</h3>
+          <ul>
+            {selectedHostel.rooms.map((room, index) => (
+              <li key={index}>
+                <p>Room {index + 1}</p>
+                <p>Standard: {room.standard}</p>
+                <p>Price: {room.price}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )},
     </div>
   );
 };
