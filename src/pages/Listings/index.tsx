443e37718f891c0d865b610ee49c486344b2dd94
moses-p <mosesharuna407@gmail.com>
2023-11-29 18:37:35 +0300

    changes

--- src/pages/Listings/index.tsx
+++ src/pages/Listings/index.tsx
@@ -1,47 +1,56 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-type hostel ={
+
+type Hostel = {
   _id: string;
-  name : string ;
-  description: string;  
-}
-const Listings = () => {
-  const [hostels, setHostels] = useState<hostel[]>();
-  const [selectedHostel, setSelectedHostel] = useState<hostel>();
+  name: string;
+  description: string;
+  // Add more properties as needed
+};
+
+const Listings: React.FC = () => {
+  const [hostels, setHostels] = useState<Hostel[] | undefined>();
+  const [selectedHostel, setSelectedHostel] = useState<Hostel | undefined>();
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchHostels = async () => {
       try {
-        // Replace the API endpoint with your actual endpoint
         const response = await axios.get('http://localhost:8800/api/hostels');
-        setHostels(response.data); // Assuming the response is an array of hostels
+        setHostels(response.data);
       } catch (error) {
         console.error('Error fetching hostels:', error);
       }
     };
 
     fetchHostels();
-  }, []); // Empty dependency array to run the effect only once on component mount
+  }, []);
 
-  const handleHostelSelect = (hostel:any) => {
+  const handleHostelSelect = (hostel: Hostel) => {
     setSelectedHostel(hostel);
+    setShowDropdown(true);
   };
-
-  console.log(hostels)
 
   return (
     <div>
       <h2>Hostel List</h2>
       <ul>
-        {hostels && hostels.map((hostel) => (
-          <li key={hostel._id}>
-            <div>
-              <h3>{hostel.name}</h3>
-              <p>{hostel.description}</p>
-              <button onClick={() => handleHostelSelect(hostel)}>Select this Hostel</button>
-            </div>
-          </li>
-        ))}
+        {hostels &&
+          hostels.map((hostel) => (
+            <li key={hostel._id}>
+              <div>
+                <h3>{hostel.name}</h3>
+                <p>{hostel.description}</p>
+                <button onClick={() => handleHostelSelect(hostel)}>Select this Hostel</button>
+              </div>
+              {showDropdown && selectedHostel && selectedHostel._id === hostel._id && (
+                <div>
+                  <h4>Dropdown for {hostel.name}</h4>
+                  {/* Add your dropdown content here */}
+                </div>
+              )}
+            </li>
+          ))}
       </ul>
 
       {selectedHostel && (
